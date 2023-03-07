8ddda8e46decbd3d122059df1ec8cc5e9162ff6b
nfrank5 <nfrankenfeld@gmail.com>
2023-03-07 16:25:39 +0800

    Add step 5

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @events = current_user.created_events
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
   get 'events/index'
-  root 'events#index'
+  get 'users/:id' => 'users#show'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'events#index'
+
 end
