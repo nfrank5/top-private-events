80f6d22d783309f073381d0749c2f29292085331
nfrank5 <nfrankenfeld@gmail.com>
2023-03-08 14:40:16 +0800

    Add steps 6, 7 and 8

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,30 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+  
+  def create
+    @user = current_user
+    @event = @user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:date, :location, :name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get 'events/index'
+  #get 'events/index'
+  resources :events
   get 'users/:id' => 'users#show'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
