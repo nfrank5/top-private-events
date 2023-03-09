93e2820eba8cab3956901bbb1fd5a0b6a987a37e
nfrank5 <nfrankenfeld@gmail.com>
2023-03-09 15:46:04 +0800

    Add steps 1 and 2 of Event Attendance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_093522) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_030605) do
   create_table "events", force: :cascade do |t|
     t.date "date"
     t.string "location"
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_093522) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "creator_id"
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.integer "attended_event_id"
+    t.integer "attendee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
