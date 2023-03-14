f7bdf53bf47aaa5cfc2167b3b084755221b13615
nfrank5 <nfrankenfeld@gmail.com>
2023-03-14 15:26:51 +0800

    Add step 2 of Finishing touches

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ApplicationRecord
 
   has_many :invitations, foreign_key: :attended_event_id
   has_many :attendees, through: :invitations, source: :attendee 
+
+
+  scope :upcoming_events, -> { where("date >= ?", DateTime.now) }
+  scope :past_events, -> { where("date < ?", DateTime.now) }
 end
